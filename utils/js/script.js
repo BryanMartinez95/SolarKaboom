5d53022b5adb6afdf2dbf3027e0ce038e4ba21c5
BryanMartinez <bryan.fm95@gmail.com>
2019-06-21 23:59:23 -0600

    animations

--- utils/js/script.js
+++ utils/js/script.js
@@ -1,6 +1,26 @@
-anime({
+// anime({
+//     targets: 'div',
+//     translateX: 250,
+//     rotate: '1turn',
+//     duration: 800
+//   });
+
+  anime({
     targets: 'div',
-    translateX: 250,
-    rotate: '1turn',
-    duration: 800
+    translateX: {
+      value: 250,
+      duration: 800
+    },
+    rotate: {
+      value: 360,
+      duration: 1800,
+      easing: 'easeInOutSine'
+    },
+    scale: {
+      value: 2,
+      duration: 1600,
+      delay: 800,
+      easing: 'easeInOutQuart'
+    },
+    delay: 250 // All properties except 'scale' inherit 250ms delay
   });
